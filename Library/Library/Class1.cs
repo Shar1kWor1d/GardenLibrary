3ece7178db133fb690ad398788bbc5f17215d705
Ломакин Кирилл <imbirka.4@yandex.ru>
2023-07-05 10:32:14 +0300

    Добавьте файлы проекта.

---
+++ Library/Library/Class1.cs
@@ -0,0 +1,42 @@
+﻿//Студент Змиев Александр
+using System;
+
+namespace GardenLibrary
+{
+    public class Garden
+    {
+        public string Address { get; set; }
+        public string Owner { get; set; }
+        public Flower[] Flowers { get; set; }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Garden Address: {Address}");
+            Console.WriteLine($"Owner: {Owner}");
+            Console.WriteLine("Flowers:");
+            foreach (var flower in Flowers)
+            {
+                flower.Show();
+            }
+        }
+    }
+
+    public class Flower : IComparable<Flower>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Flower Title: {Title}");
+            Console.WriteLine($"Description: {Description}");
+        }
+
+        public int CompareTo(Flower other)
+        {
+            // Реализуйте сравнение двух цветов по вашему выбранному критерию.
+            // Например, сравнение по названию цветка (Title).
+            return string.Compare(Title, other.Title, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
---
+++ LibraryGarden/LibraryGarden/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using GardenLibrary; // Добавлена ссылка на библиотеку с классами Garden и Flower
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта класса Garden
+            Garden garden = new Garden
+            {
+                Address = "Полярная ул. 23",
+                Owner = "Змиев Александр", // Фамилия учащегося
+                Flowers = new Flower[]
+                {
+                    new Flower { Title = "Роза", Description = "Удивительный красный цветок" },
+                    new Flower { Title = "Тюльпан", Description = "Красочный весенний цветок" },
+                    new Flower { Title = "Подсолнух", Description = "Яркий и жизнерадостный цветок" }
+                }
+            };
+
+            // Показ содержимого сада
+            garden.ShowAll();
+
+            // Пример использования метода CompareTo()
+            Flower flower1 = new Flower { Title = "Роза", Description = "Удивительный красный цветок" };
+            Flower flower2 = new Flower { Title = "Тюльпан", Description = "Красочный весенний цветок" };
+
+            //Выполнение сравнения двух объектов
+            int comparisonResult = flower1.CompareTo(flower2);
+            Console.WriteLine($"Результат comparison: {comparisonResult}");
+
+            Console.ReadLine();
+        }
+    }
+}
